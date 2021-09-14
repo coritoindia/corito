bf2666dc5c20a11e1b05398ae64e02320f6edb95
coritoindia <coritoindia@gmail.com>
2021-09-15 01:21:26 +0530

    Updates

---
+++ precache-manifest.e7934bbfc875c3d8c5e836c87252aae1.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3bcc061cc11a5acca5ad4600c688e75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ccbdd2f01057ece9e04",
+    "url": "/static/css/2.e3e4b781.chunk.css"
+  },
+  {
+    "revision": "82eab5fd098591b3ca12",
+    "url": "/static/css/main.4a9cf335.chunk.css"
+  },
+  {
+    "revision": "3ccbdd2f01057ece9e04",
+    "url": "/static/js/2.a3ba2ab1.chunk.js"
+  },
+  {
+    "revision": "308d2d97fd519422d20953c3e51f94e3",
+    "url": "/static/js/2.a3ba2ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82eab5fd098591b3ca12",
+    "url": "/static/js/main.134e5619.chunk.js"
+  },
+  {
+    "revision": "6f49d9ca10ad72745a4f",
+    "url": "/static/js/runtime-main.677d8afb.js"
+  },
+  {
+    "revision": "296942da1148169d0f2dad5e62351aa9",
+    "url": "/static/media/12.296942da.svg"
+  },
+  {
+    "revision": "d235de224173c57fe71b478afc659dd6",
+    "url": "/static/media/60.d235de22.svg"
+  },
+  {
+    "revision": "36f3011b9372f693c4ac6fa4062c75d3",
+    "url": "/static/media/74.36f3011b.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "17ad320124c2e9e3e96e06890d6cf0cd",
+    "url": "/static/media/hero2.17ad3201.svg"
+  },
+  {
+    "revision": "6b83fa910dca69a4cb9317509769b339",
+    "url": "/static/media/logo.6b83fa91.svg"
+  },
+  {
+    "revision": "eb72033e7d9761a9ddc635b3a6b25441",
+    "url": "/static/media/services.eb72033e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/corito/precache-manifest.5e2cfe438ddc7248dfe9f5b8c5ac6ae0.js"
+  "/precache-manifest.e7934bbfc875c3d8c5e836c87252aae1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/corito/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
